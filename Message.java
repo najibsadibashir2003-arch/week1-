9b1258212cae7a14619d2ccf1c26365d23cc9c2f
najibsadibashir2003-arch <najibsadibashir2003@gmail.com>
2026-04-15 15:07:04 +0100

    Delete Message.java

--- Message.java
+++
@@ -1,6 +0,0 @@
-public class Message{
-    public static void main(String[] args){
-        System.out.println("My Name is salihu garba muhammad");
-        System.out.println("I am learning java");
-    }
-}
