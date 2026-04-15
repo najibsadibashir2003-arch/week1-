cbebe24241b6c9597a731a3a80172b721ef7b7f0
najibsadibashir2003-arch <najibsadibashir2003@gmail.com>
2026-04-15 15:00:50 +0100

    Delete Programming.java

--- Programming.java
+++
@@ -1,9 +0,0 @@
-public class Programming{
-    public static void main(String[] args){
-        System.out.println("Wellcome to Programming");
-        System.out.println("=======================");
-
-        
-     }
-    
-}
