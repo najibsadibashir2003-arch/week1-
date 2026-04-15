6a9afabaafd884f0776272d10011a1aaa8075185
najibsadibashir2003-arch <najibsadibashir2003@gmail.com>
2026-04-15 15:02:33 +0100

    Delete Multiplication.java

--- Multiplication.java
+++
@@ -1,8 +0,0 @@
-public class Multiplication{
-    public static void main(String[] args){
-        int a=4;
-        int b=6;
-        System.out.println("The Multiplication is "+(a*b));
-    }
-
-}
