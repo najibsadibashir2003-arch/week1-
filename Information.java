3f6999f5b6284eeac078447bcaf476938712a830
najibsadibashir2003-arch <najibsadibashir2003@gmail.com>
2026-04-15 15:08:01 +0100

    Delete Information.java

--- Information.java
+++
@@ -1,7 +0,0 @@
-public class Information{
-public static void main(String[] args){
-    System.out.println("My is salihu garba muhammad");
-    System.out.println("Depatment software emgineering");
-    System.out.println("My institution wakilin bagwai investment");
-}    
-}
