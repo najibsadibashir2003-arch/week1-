37dc56f78891ce3f8e39947a1aabfda7d638bd88
najibsadibashir2003-arch <najibsadibashir2003@gmail.com>
2026-04-15 14:56:04 +0100

    Delete Data.java

--- Data.java
+++
@@ -1,8 +0,0 @@
-public class Data{
-    public static void main(String[] args){
-        System.out.println("=====Bio Data=====");
-        System.out.println("Name____");
-        System.out.println("Couse____");
-        System.out.println("Level____");
-    }
-    }
